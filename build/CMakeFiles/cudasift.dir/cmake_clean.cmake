1cb3772bc6bedd2a15dc9a21018c533572fc5a40
Konstantinos Zampogiannis <kzampog@gmail.com>
2017-09-19 20:05:41 -0400

    Reorganization

---
+++ build/CMakeFiles/cudasift.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuda_compile.dir/src/cuda_compile_generated_cudaImage.cu.o"
+  "CMakeFiles/cuda_compile.dir/src/cuda_compile_generated_matching.cu.o"
+  "CMakeFiles/cuda_compile.dir/src/cuda_compile_generated_cudaSiftHD.cu.o"
+  "CMakeFiles/cudasift.dir/src/mainSift.cpp.o"
+  "CMakeFiles/cudasift.dir/src/geomFuncs.cpp.o"
+  "cudasift.pdb"
+  "cudasift"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cudasift.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
